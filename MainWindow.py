0b1ed97402fc33c667fc4e4a9ae7750ebfff6922
Hatem Adel Alshafaei <halshafaei@gmail.com>
2019-09-11 14:24:41 +0200

    Add files via upload

--- MainWindow.py
+++ MainWindow.py
@@ -28,26 +28,41 @@ class MainWindow:
         frameEntries = Frame(self.window)
         Label(frameEntries, text='Username').grid(row=0)
         Label(frameEntries, text='Password').grid(row=1)
-        e1 = Entry(frameEntries).grid(row=0, column=1)
-        e2 = Entry(frameEntries).grid(row=1, column=1)
+        e1 = Entry(frameEntries)
+        e1.grid(row=0, column=1)
+        e2 = Entry(frameEntries, show="*")
+        e2.grid(row=1, column=1)
+        self.Username = e1
+        self.Password = e2
 
         frameButton = Frame(self.window)
-        button = Button(frameButton, text='Go!', width=25, command=self.switch_Window)
+        button = Button(frameButton, text='Go!', width=25, command=self.login_check)
         button.grid(row=1)
 
+        self.window.grid_rowconfigure(1, weight=1)
+        self.window.grid_rowconfigure(0, weight=1)
         frameEntries.grid(row=0)
         frameButton.grid(row=1)
-    def switch_Window(self):
+
+    def switch_window(self):
         self.newWindow = tk.Toplevel(self.window)
-        Finance_Window(self.newWindow,self.window)
+        Finance_Window(self.newWindow, self.window)
         self.window.withdraw()
 
+    # check passwords
+    def login_check(self):
+        print(self.Username.get())
+        print(self.Password.get())
+        if self.Username.get() == "root":
+            if self.Password.get() == "root":
+                self.switch_window()
 
 
 def main():
     main_window = Tk()
     MainWindow(main_window)
     main_window.mainloop()
+
 
 if __name__ == '__main__':
     main()
